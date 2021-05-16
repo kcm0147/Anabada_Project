5f1913d615458c7ea2d969ed03dc1b39f1623430
nowwater <6360879@naver.com>
2021-05-16 22:54:39 +0900

    fix: saleItem domain

--- backend/src/main/java/com/auction/anabada/domain/SaleItem.java
+++ backend/src/main/java/com/auction/anabada/domain/SaleItem.java
@@ -1,2 +1,24 @@
-package com.auction.anabada.domain;public class SaleItem {
+package com.auction.anabada.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Entity
+@Getter
+@NoArgsStructor
+public class SaleItem {
+
+    @Id @GeneratedValue
+    @Column(name="sale_item_id")
+    Long id;
+
+    @OneToOne(mappedBy = "saleItem")
+    Item item;
+
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    User seller;
 }
