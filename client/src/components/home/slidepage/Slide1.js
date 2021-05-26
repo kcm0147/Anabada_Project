cc89e8c36454fb95eb741029a6a3af82a8b5f85f
o-star <uh04049@naver.com>
2021-05-26 19:37:47 +0900

    home slidebox view complete

--- client/src/components/home/slidepage/Slide1.js
+++ client/src/components/home/slidepage/Slide1.js
@@ -4,7 +4,7 @@ import Slide1Img from 'assets/image/slide1.png'
 export default function Slide1() {
     return (
         <div className='entire-slide1'>
-            <div id='desc-section'>
+            <div>
                 <div id='title'>
                     합리적인 가격에 사고 팔자
                 </div>
--- client/src/components/home/slidepage/Slide3.js
+++ client/src/components/home/slidepage/Slide3.js
@@ -1,9 +1,20 @@
-import 'assets/css/Slide3.css'
+import 'assets/css/Slide3.scss'
+import Slide3Img from 'assets/image/slide3.svg'
 
 export default function Slide1() {
     return (
         <div className='entire-slide3'>
-
+            <div>
+                <div id='title'>
+                    나의 물건을 더 값지게
+                </div>
+                <div>
+                    내 쓸모 없는 물건이 <br />
+                    누구에겐 필요한 물건으로 <br />
+                    내 소장품들을 더 높은 가치로 평가받아보세요
+                </div>
+            </div>
+            <img src={Slide3Img} />
         </div>
     );
 }
