7782543adde71586815659c1be5e5a0810f3f0a8
o-star <uh04049@naver.com>
2021-05-26 22:22:50 +0900

    popular search section update

---
+++ client/src/components/home/PopularSearch.js
@@ -0,0 +1,19 @@
+import 'assets/css/PopularSearch.scss'
+
+export default function PopularSearch() {
+    return (
+        <div id='popular-search-section'>
+            <div id='popular-search-maintitle'>
+                인기 경매 검색어
+            </div>
+            <div id='rank-section'>
+                <span>마르지엘라</span>
+                <span>자전거</span>
+                <span>나이키</span>
+                <span>장식품</span>
+                <span>신발</span>
+                <span>루이비똥</span>
+            </div>
+        </div>
+    )
+}
