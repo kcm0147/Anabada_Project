2b88b655d9d19952383cdffcce87a343e2f29384
nowwater <6360879@naver.com>
2021-06-06 14:28:39 +0900

    fix: LIKE Query for retrieving item by name

--- backend/src/main/java/com/auction/anabada/item/repository/ItemRepository.java
+++ backend/src/main/java/com/auction/anabada/item/repository/ItemRepository.java
@@ -1,17 +1,18 @@
 package com.auction.anabada.item.repository;
 
 import com.auction.anabada.item.domain.Item;
-import com.auction.anabada.item.dto.ItemDto;
 import com.auction.anabada.user.domain.Category;
 import java.util.List;
 import javax.persistence.EntityManager;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 @Transactional
 @RequiredArgsConstructor
+@Slf4j
 public class ItemRepository {
 
     private final EntityManager em;
@@ -45,15 +46,15 @@ public class ItemRepository {
             sql += "'" + categories.get(i) + "',";
         }
         sql = sql.substring(0, sql.length() - 1) + ")";
-        System.out.println("실행된 SQL :  "+ sql);
+        log.info("실행된 SQL :  "+ sql);
         return em.createQuery(sql, Item.class).getResultList();
     }
 
     @Transactional(readOnly = true)
     public List<Item> findWithItemName(String includedName){
-        String sql = "select i from Item i where i.itemName LIKE ?1";
+        String sql = "select i from Item i where i.itemName LIKE \'%" + includedName + "%\'";
+        log.info("실행된 SQL :  "+ sql);
         return em.createQuery(sql, Item.class)
-            .setParameter(1, includedName)
             .getResultList();
     }
 
