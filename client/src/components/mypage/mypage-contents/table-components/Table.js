ca091f7ee6d82298e39719a28833e14e8c5e1786
o-star <uh04049@naver.com>
2021-06-08 21:52:53 +0900

    mypage & itemdetailview connect

--- client/src/components/mypage/mypage-contents/table-components/Table.js
+++ client/src/components/mypage/mypage-contents/table-components/Table.js
@@ -1,6 +1,13 @@
 import 'assets/css/Table.scss'
+import { useHistory } from 'react-router-dom'
 
 export default function Table({ headary, contentary, resultPage }) {
+
+    const history = useHistory();
+
+    const onClickTableRow = (e) => {
+        history.push(`/item/${e.target.parentElement.id}`)
+    }
 
     const renderingContent = () => {
         const result = [];
@@ -13,22 +20,24 @@ export default function Table({ headary, contentary, resultPage }) {
         //     base64 = reader.result;
         // }
 
-        if (resultPage)
+        if (resultPage) {
             for (const comp of contentary) {
 
-                result.push(<tr key={key}>
-                    <td>{comp.itemImage}</td>
-                    <td>{comp.itemName}</td>
-                    <td>{comp.category}</td>
-                    <td>{comp.lastAuctionDate}</td>
-                    <td>{comp.lastAuctionPrice}</td>
-                    <td>{comp.lastUserPrice}</td>
-                    <td>{comp.result ?
-                        <span className='success-span'>낙찰</span> :
-                        <span className='fail-span'>패찰</span>}</td>
-                </tr>)
+                result.push(
+                    <tr key={key} id={comp.itemId} onClick={onClickTableRow}>
+                        <td>{comp.itemImage}</td>
+                        <td>{comp.itemName}</td>
+                        <td>{comp.category}</td>
+                        <td>{comp.lastAuctionDate}</td>
+                        <td>{comp.lastAuctionPrice}</td>
+                        <td>{comp.lastUserPrice}</td>
+                        <td>{comp.result ?
+                            <span className='success-span'>낙찰</span> :
+                            <span className='fail-span'>패찰</span>}</td>
+                    </tr>)
                 key++;
             }
+        }
 
         else {
             for (const comp of contentary) {
@@ -40,14 +49,15 @@ export default function Table({ headary, contentary, resultPage }) {
                 //     console.log(reader.result);
                 // }
 
-                result.push(<tr key={key}>
-                    <td>{comp.itemImage}</td>
-                    <td>{comp.itemName}</td>
-                    <td>{comp.category}</td>
-                    <td>{comp.lowerBoundPrice}</td>
-                    <td>{comp.auctionStartDate}</td>
-                    <td>{comp.auctionEndDate}</td>
-                </tr>)
+                result.push(
+                    <tr key={key} id={comp.itemId} onClick={onClickTableRow}>
+                        <td>{comp.itemImage}</td>
+                        <td>{comp.itemName}</td>
+                        <td>{comp.category}</td>
+                        <td>{comp.lowerBoundPrice}</td>
+                        <td>{comp.auctionStartDate}</td>
+                        <td>{comp.auctionEndDate}</td>
+                    </tr >)
                 key++;
             }
         }
