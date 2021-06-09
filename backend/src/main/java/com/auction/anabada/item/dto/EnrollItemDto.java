e7ec60d838d119bf3b2f908c3d5f8c0926fcc9c0
kcm0147 <kcm0147@hanmail.net>
2021-06-09 12:03:11 +0900

    fix: fix imagePath

--- backend/src/main/java/com/auction/anabada/item/dto/EnrollItemDto.java
+++ backend/src/main/java/com/auction/anabada/item/dto/EnrollItemDto.java
@@ -31,15 +31,17 @@ public class EnrollItemDto {
      public void makeImagePath(String path) {
         path = path +File.separator + "static" + File.separator + "images"; // 이미지 업로드 폴더 /resources/static/images
         log.info("이미지 저장 경로 : " + path);
+        String fileName;
         String filePath;
         String absolutePath = System.getProperty("user.dir");;
 
         if(this.imageFile.isEmpty()) { // 애초에 모든 물품은 사진 등록을 완료해야 함
             log.error("파일이 첨부되지 않았습니다.");
-            filePath = path + File.separator + "default.jpg";
+            fileName ="default.jpg";
         } else { // 파일이 존재할 경우 정상 로직 수행
             log.info("파일이 정상적으로 첨부되었습니다.");
-            filePath= absolutePath+path+'/'+System.nanoTime()+imageFile.getOriginalFilename();
+            fileName=System.nanoTime()+imageFile.getOriginalFilename();
+            filePath= absolutePath+path+'/'+fileName;
 
             File dir = new File(absolutePath+path);
             File target = new File(filePath);
@@ -58,8 +60,8 @@ public class EnrollItemDto {
             }
         }
 
-        log.info("파일이 저장된 경로 : " + filePath);
-        this.setImagePath(filePath);
+        log.info("파일이 저장된 경로 : " + fileName);
+        this.setImagePath(fileName);
     }
 
 }
