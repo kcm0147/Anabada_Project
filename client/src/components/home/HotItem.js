fe7d720336034be551619e8f39ff1cdf593b3411
o-star <uh04049@naver.com>
2021-06-12 00:31:26 +0900

    style : item list view style update

--- client/src/components/home/HotItem.js
+++ client/src/components/home/HotItem.js
@@ -3,14 +3,14 @@ import { Col } from "react-bootstrap";
 import "assets/css/Item.scss";
 
 const HotItem = ({ name, img }) => {
-  return (
-    <Col className="item-style">
-      <div>
-        <img src={img} alt="상품 이미지" width="200px" height="200px" />
-        <div className="item-description">{name}</div>
-      </div>
-    </Col>
-  );
+    return (
+        <Col className="item-style">
+            <div>
+                <img src={img} alt="상품 이미지" width="200px" height="200px" />
+                <div className="item-description">{name}</div>
+            </div>
+        </Col>
+    );
 };
 
 export default HotItem;
--- client/src/components/home/HotItemView.js
+++ client/src/components/home/HotItemView.js
@@ -6,35 +6,35 @@ import HotItem from "components/home/HotItem";
 import "assets/css/HotItemView.scss";
 
 const HotItemView = () => {
-  const dispatch = useDispatch();
-  const bestItemsData = useSelector((s) => s.ITEM.data);
+    const dispatch = useDispatch();
+    const bestItemsData = useSelector((s) => s.ITEM.data);
 
-  useEffect(() => {
-    dispatch(getBest8ItemsRequest());
-  }, [dispatch]);
+    useEffect(() => {
+        dispatch(getBest8ItemsRequest());
+    }, [dispatch]);
 
-  return (
-    <div id="hot-item-section">
-      <div id="sub-item-section">
-        <div id="main-title">인기 경매품 목록</div>
-        <ul id="row-style">
-          {!bestItemsData ? (
-            <p>"인기 경매품 로딩중..."</p>
-          ) : (
-            bestItemsData.map((item, idx) => (
-              <Link to={`/item/${item.itemId}`}>
-                <HotItem
-                  name={item.itemName}
-                  img={`http://18.222.240.132:8089/images/${item.itemImage}`}
-                  key={idx}
-                />
-              </Link>
-            ))
-          )}
-        </ul>
-      </div>
-    </div>
-  );
+    return (
+        <div id="hot-item-section">
+            <div id="sub-item-section">
+                <div id="main-title">인기 경매품 목록</div>
+                <ul id="row-style">
+                    {!bestItemsData ? (
+                        <p>"인기 경매품 로딩중..."</p>
+                    ) : (
+                        bestItemsData.map((item, idx) => (
+                            <Link to={`/item/${item.itemId}`}>
+                                <HotItem
+                                    name={item.itemName}
+                                    img={`http://18.222.240.132:8089/images/${item.itemImage}`}
+                                    key={idx}
+                                />
+                            </Link>
+                        ))
+                    )}
+                </ul>
+            </div>
+        </div>
+    );
 };
 
 export default HotItemView;
